6be5933c25c6d3a9f066d01c702011e44322c97b
Nate <namoricoo@hotmail.com>
2014-01-12 13:04:15 -0800

    add hidden home page for testing quakismart

--- app/controllers/usgs_controller.rb
+++ app/controllers/usgs_controller.rb
@@ -22,7 +22,29 @@ class UsgsController < ApplicationController
       marker.lat earthquake.latitude
       marker.lng earthquake.longitude
     end    
-  end 
+  end
+  
+   def hidden
+     # empty out the table for each new visit.
+    initialize_table
+    
+    earthquake_class = EarthquakeClass.new
+    @table_header =  earthquake_class.get_table_header
+    
+    file_name = 'sample_earthquake.geojson'
+    geo = GeoJsonHhelper.new
+    result = geo.get_local_json_file(file_name)
+    result_hash = geo.get_array_earthquake_hash(result)
+    @table_body = geo.set_earthquake_features(result_hash)
+    seed_database_with_data(@table_body)
+    
+    
+    @earthquakes = Earthquake.all
+    @earthquake_hash = Gmaps4rails.build_markers(@earthquakes) do |earthquake, marker|
+      marker.lat earthquake.latitude
+      marker.lng earthquake.longitude
+    end
+   end  
 
   def seed_database_with_data(seed_hash_array)
     seed_hash_array.each do | hash_array |  
